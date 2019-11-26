6a37145baeaa0198092613cc2fc6d8f7ab672dd0
peruljain <perul365@gmail.com>
2019-11-26 15:17:45 +0530

    ccc

--- project/settings.py
+++ project/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
@@ -144,8 +148,6 @@ EMAIL_HOST_USER = 'perul1999@gmail.com'
 EMAIL_HOST_PASSWORD = 'kajalbharti'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 django_heroku.settings(locals())
